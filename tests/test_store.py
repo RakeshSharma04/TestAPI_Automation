40879ca4a006073c83ae091a4de68e8cd6810bff
Rakesh <rakeshmandavya@gmail.com>
2025-04-29 07:53:54 +0530

    testing cicd pipline

--- tests/test_store.py
+++ tests/test_store.py
@@ -3,17 +3,20 @@ from api.store_api import StoreAPI
 from payloads.order_payload import create_sample_order
 from responses.save_response import save_response_to_file
 
+
 @pytest.fixture(scope="module")
 def store_api():
     return StoreAPI()
+
 
 def test_get_inventory(store_api):
     response = store_api.get_inventory()
     assert response.status_code == 200
     save_response_to_file(response, 'inventory_response.json')
 
+
 def test_create_order(store_api):
     payload = create_sample_order()
     response = store_api.create_order(payload)
-    assert response.status_code == 200
+    assert response.status_code == 200 # assert if status is 200
     save_response_to_file(response, 'create_order_response.json')
